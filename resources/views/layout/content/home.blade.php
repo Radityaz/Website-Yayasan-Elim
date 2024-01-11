e3caedaf552da5a6810228f68f9868d45582b0e6
Your Name <you@example.com>
2024-01-11 09:36:04 +0700

    [Riko] Adding Home Wireframe

--- resources/views/layout/content/home.blade.php
+++ resources/views/layout/content/home.blade.php
@@ -1,6 +1,20 @@
 @extends('layout.main')
 @section('content')
-    <div class="container">
-        <h1>Ini Beranda</h1>
+    <div class="container-fluid mt-5">
+        <div class="row vh-100">
+            <div class=" offset-1 col-5 d-flex flex-column align-items-start justify-content-center">
+                <h1 class="font-hero" >Great School</h1>
+                <h2>For The Best Student</h2>
+                <p class="font-xxs w-50 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                <button class="btn font-s w-25 text-start rounded-button rounded-pill bg-primary " > Baca Lebih Lanjut </button>
+            </div>
+            <div class="col-6 d-flex align-items-center justify-content-end p-0 m-0">
+                <img height="620" width="620" src="/image/HeroBackground.png"alt="" srcset="">
+            </div>
+        </div>
     </div>
+    <div class="cotainer-fluid background-photo vh-100 d-flex">
+        
+    </div>
+    
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,7 +10,7 @@
     <link rel="shortcut icon" href="/image/logo.png" type="image/x-icon">
 </head>
 <body>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
         <div class="container">
           <a class="navbar-brand p-0 " href="/">
             <img class="p-0 m-0" src="/image/logo.png"  width=60" height="55">
@@ -18,16 +18,16 @@
           <div class="collapse navbar-collapse " id="navbarNavDropdown">
             <ul class="navbar-nav ms-auto ">
               <li class="nav-item navbar-active">
-                <a class="nav-link font-xs hover-grow" href="/">Beranda</a>
+                <a class="nav-link font-s hover-grow" href="/">Beranda</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link font-xs " href="/about">Tentang Kami</a>
+                <a class="nav-link font-s " href="/about">Tentang Kami</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link font-xs " href="/article">Artikel</a>
+                <a class="nav-link font-s " href="/article">Artikel</a>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link font-xs  dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link font-s  dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Tempat
                 </a>
                 <ul class="dropdown-menu">
@@ -41,9 +41,8 @@
         </div>
       </nav>
 
-      <div class="container-fluid">
-        @yield('content')
-      </div>
+
+      @yield('content')
       
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
